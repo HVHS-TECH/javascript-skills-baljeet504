de9ea460c11c6a3a12b05cfa65e6420c1f26e07d
baljeet504 <26527ns@hvhs.school.nz>
2026-04-29 00:00:54 +0000

    Apr 29, 2026, 12:00 AM

--- t01_introduction.js
+++ t01_introduction.js
@@ -3,7 +3,7 @@ task1
 ****************************/
 console.log("Running t01_introduction.js")
 console.log('Hello world')
-console.log('i am the crewmate the good huy of the spaceship')
+console.log('i am the crewmate the good guy of the spaceship')
 
 // Variables
 
