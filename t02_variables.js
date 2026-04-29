077ad1da51090e0d226d469f3ac8f58221d7b37f
baljeet504 <26527ns@hvhs.school.nz>
2026-04-29 00:10:55 +0000

    Apr 29, 2026, 12:10 AM

--- t02_variables.js
+++ t02_variables.js
@@ -6,13 +6,14 @@ console.log("Running t02_variables.js")
 
 // Variables
 let userName='nehan';
-let userAge='14';
+let userAge=14;
+let userMoney=0;
 /***************************
 Main code
 ****************************/
 console.log(userName);
 console.log(userAge);
-
+console.log(userMoney);
 /****************************
  function
  ****************************/
