e491f9b5ac33cf32c02c8d519d7cf18f5a2759b5
baljeet504 <26527ns@hvhs.school.nz>
2026-04-29 00:04:44 +0000

    Apr 29, 2026, 12:04 AM

--- t02_variables.js
+++ t02_variables.js
@@ -1,1 +1,16 @@
-/* Paste your code from task 1 here */
+/* Paste your code from task 1 here *//****************************
+task1
+****************************/
+console.log("Running t02_variables.js")
+
+
+// Variables
+
+
+/***************************
+Main code
+****************************/
+
+/****************************
+ function
+ ****************************/
