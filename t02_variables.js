a630a09018c4a89c7cb154840fcf0ddeab907e75
baljeet504 <26527ns@hvhs.school.nz>
2026-04-30 01:14:00 +0000

    Apr 30, 2026, 1:14 AM

--- t02_variables.js
+++ t02_variables.js
@@ -21,9 +21,9 @@ console.log(userYear);
 
 console.log('Users name is '+ userName)
 console.log('Users age is '+ userAge)
-console.log(2+2);
-console.log(userNum+1)
-console.log(userOthernum+userNum)
+console.log("add", 2+2);
+console.log("subrtract",  userNum-1)
+console.log("multiply",  userOthernum*userNum)
 /****************************
  function
  ****************************/
