cb2e7ea047b8674a5cfff86149431e0a8b8d4176
baljeet504 <26527ns@hvhs.school.nz>
2026-05-08 02:59:42 +0000

    May 8, 2026, 2:59 AM

--- t05_JavaScript_and_HTML.js
+++ t05_JavaScript_and_HTML.js
@@ -1,8 +1,6 @@
 let userNum=9;
 let userOthernum=18;
 
-console.log('Users name is '+ userName);
-console.log('Users age is '+ userAge);
 console.log("add", 2+2);
 console.log("subrtract",  userNum-1);
 console.log("multiply",  userOthernum*userNum);
