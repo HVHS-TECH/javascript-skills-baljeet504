04033973e6957e6443d29b6332b365297e0eecd6
baljeet504 <26527ns@hvhs.school.nz>
2026-05-08 02:40:34 +0000

    May 8, 2026, 2:40 AM

--- t04_debugging.js
+++ t04_debugging.js
@@ -15,7 +15,8 @@ console.log("A area of a piece of paper with a length of ="length"+, and a width
 console.log("The same piece of paper has a perimeter of "+perimeter)
 
 // Check, does this result look sensible
-if (perimeter > area)
-    console.log("Yay, your area is larger, this is right for this example");
+if (perimeter > area){
+    console.log("Yay, your area is larger, this is right for this example")
+}else{
     console.log("Hmm, your perimeter is larger. It shouldn't be for this example);
-
+}
