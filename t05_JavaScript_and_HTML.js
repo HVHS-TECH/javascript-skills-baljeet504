86b135d00ce76ebf316768fdc91ef2da6e431a33
baljeet504 <26527ns@hvhs.school.nz>
2026-05-14 01:25:11 +0000

    May 14, 2026, 1:25 AM

--- t05_JavaScript_and_HTML.js
+++ t05_JavaScript_and_HTML.js
@@ -1,4 +1,47 @@
 const OUTPUT = document.getElementById("spaceForJavaScriptOutput")
-OUTPUT.innerHTML = "<h2></h2>";
+OUTPUT.innerHTML = "<h2>Won't the devil guide your hand, for good luck
+To hold the honeysuckle rose, as she lies down
+Reading the paper, reading the world right 'round
+And her eyes record the sweet surprise of those
+Who died that day
+Leaving her breathless, leading her hand to the grave
+And Jane
+You're early
+Your life's work is dirtied by the
+Fools
+Who adore you
+Only to find, only to find you out
+They saw you
+Dressing in the backroom
+Now they'll pay what they owe you
+It's only small change, red on the green, green grass
+Won't the devil take you back for more
+To open closed doors
+And keep the good from the great
+Evil in equal, seeking to kill and create
+How grand? The milk and honey land
+Is on her tongue again
+Taste of the violence, trying to silence her head
+And Jane
+You're early
+Your life's work is dirtied by the
+Fools
+Who adore you
+Biding your time, biding your time to strike
+Surely
+The poison makes a portrait of your
+Face
+In the mirror
+Smiling with fright
+And Jane
+You're early
+Your life's work is dirtied by the
+Fools
+Who adore you
+Only to find, only to find you out
+They saw you
+Dressing in the backroom
+Now they'll pay what they owe you
+It's only small change, red on the green, green grass</h2>";
 
 
