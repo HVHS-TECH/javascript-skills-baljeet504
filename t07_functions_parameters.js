d2e019965fd6fae4528c437d611af379c343f334
baljeet504 <26527ns@hvhs.school.nz>
2026-05-18 21:49:53 +0000

    May 18, 2026, 9:49 PM

--- t07_functions_parameters.js
+++ t07_functions_parameters.js
@@ -1,7 +1,6 @@
 
 function displayProduct(_name,_price){
-  OUTPUT.innerHTML += "<p>"welcome to gubland "+_price</p>";
-  console.log(`{_name}: {_price}`);
+  OUTPUT.innerHTML += "<p>"gubland"</p>";
 }
 }
 
