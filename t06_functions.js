3606d3b708463e768aa5c729900ba51138fe5f3d
baljeet504 <26527ns@hvhs.school.nz>
2026-05-18 21:14:35 +0000

    May 18, 2026, 9:14 PM

--- t06_functions.js
+++ t06_functions.js
@@ -1,7 +1,5 @@
-/****************************
-Functions
-****************************/
+
 function writeLine(){
-  // Add a line to the html page
+  
   OUTPUT.innerHTML += "<p>sum like that</p>";
 }
