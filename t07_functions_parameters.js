272895f3215121706da5c162904c9849e19dfb93
baljeet504 <26527ns@hvhs.school.nz>
2026-05-18 21:45:14 +0000

    May 18, 2026, 9:45 PM

--- t07_functions_parameters.js
+++ t07_functions_parameters.js
@@ -1,5 +1,10 @@
 
-function displayWelcome(_price){
+function displayProduct(_name,_price){
   OUTPUT.innerHTML += "<p>"welcome to gubland "+_price</p>";
+  console.log(`{_name}: {_price}`);
 }
-writeLine()
+}
+
+displayProduct("Chocolate bar", 4);
+displayProduct("Chips", 3);
+displayProduct("Drink", 2.50);
