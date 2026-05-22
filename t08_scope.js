b615c15ad11ced68cc2599c749ed8839aa6ee9f6
baljeet504 <26527ns@hvhs.school.nz>
2026-05-22 02:43:06 +0000

    May 22, 2026, 2:43 AM

--- t08_scope.js
+++ t08_scope.js
@@ -3,14 +3,14 @@ const output = document.getElementById("output");
 /* Read the slides, make a prediction, then uncomment this code.
 
 
-var myVar = 0;
-output.innerHTML = "Start: "+myVar+"<br>";
-
-functionOne();
-function functionOne() {
-  myVar = 1;
-  output.innerHTML += "functionOne: "+myVar+"<br>";
-}
+ /* var myVar = 0; */
+o/* utput.innerHTML = "Start: "+myVar+"<br>"; */
+ /*  */
+f/* unctionOne(); */
+f/* unction functionOne() { */
+ /*  myVar = 1; */
+ /*  output.innerHTML += "functionOne: "+myVar+"<br>"; */
+} 
 
 functionTwo();
 function functionTwo() {
@@ -21,4 +21,3 @@ function functionTwo() {
 output.innerHTML += "end: "+myVar+"<br>";
 
 
-*/
