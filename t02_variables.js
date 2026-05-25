ba44906835e3a25bfc72c38f5f371c618b88590f
baljeet504 <26527ns@hvhs.school.nz>
2026-05-25 21:29:03 +0000

    May 25, 2026, 9:29 PM

--- t02_variables.js
+++ t02_variables.js
@@ -1,6 +1,7 @@
-/* Paste your code from task 1 here *//****************************
-task1
-****************************/
+console.log("Running t01_introduction.js")
+console.log('Hello world')
+console.log('i am the crewmate the good guy of the spaceship')
+
 console.log("Running t02_variables.js");
 
 
--- t03_maths.js
+++ t03_maths.js
@@ -1,3 +1,28 @@
+console.log("Running t01_introduction.js")
+console.log('Hello world')
+console.log('i am the crewmate the good guy of the spaceship')
+
+console.log("Running t02_variables.js");
+
+
+// Variables
+let userName='nehan';
+let userAge=14;
+let userMoney=0;
+let userYear=2026;
+
+/***************************
+Main code
+****************************/
+console.log(userName);
+console.log(userAge);
+console.log(userMoney);
+console.log(userYear);
+
+
+/****************************
+ function
+ ****************************/
 let userNum=9;
 let userOthernum=18;
 
