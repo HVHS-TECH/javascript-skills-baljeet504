14e8cdc7d1bb744413964f20e13e12cbd84d5e79
baljeet504 <26527ns@hvhs.school.nz>
2026-05-27 00:15:19 +0000

    May 27, 2026, 12:15 AM

--- t11_data_types.js
+++ t11_data_types.js
@@ -1,1 +1,51 @@
-/* Paste your code from the last task */
+console.log("Running t01_introduction.js")
+console.log('Hello world')
+console.log('i am the crewmate the good guy of the spaceship')
+
+console.log("Running t02_variables.js");
+
+
+// Variables
+let userName='nehan';
+let userAge=14;
+let userMoney=0;
+let userYear=2026;
+
+/***************************
+Main code
+****************************/
+console.log(userName);
+console.log(userAge);
+console.log(userMoney);
+console.log(userYear);
+
+
+/****************************
+ function
+ ****************************/
+let userNum=9;
+let userOthernum=18;
+
+console.log('Users name is '+ userName);
+console.log('Users age is '+ userAge);
+console.log("add", 2+2);
+console.log("subrtract",  userNum-1);
+console.log("multiply",  userOthernum*userNum);
+
+const OUTPUT = document.getElementById("spaceForJavaScriptOutput")
+
+OUTPUT.innerHTML = "<h2>name</h2>";
+
+function writeLine(){
+  const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
+  
+  OUTPUT.innerHTML += "<p>sum like that</p>";
+}
+writeLine()
+
+function getFormInput(){
+  const NAME_FIELD = document.getElementById("nameField");
+  let userName = NAME_FIELD.value;
+  OUTPUT.innerHTML = "<p>Your name is "+userName+"</p>";
+}
+
