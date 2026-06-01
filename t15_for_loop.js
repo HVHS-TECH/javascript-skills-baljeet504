cd68364f75be5cb72e58a81f0779b08f4341266b
baljeet504 <26527ns@hvhs.school.nz>
2026-06-01 09:31:58 +0000

    Jun 1, 2026, 9:31 AM

--- t15_for_loop.js
+++ t15_for_loop.js
@@ -1,3 +1,4 @@
+
 for(let i=1; i<100; i++){
   console.log("bottles of milk on the wall : "+i);
 }
--- t16_arrays_continued.js
+++ t16_arrays_continued.js
@@ -1,1 +1,3 @@
-/* Paste your code from the last task */
+for(let i=1; i<100; i++){
+  console.log("bottles of milk on the wall : "+i);
+}
