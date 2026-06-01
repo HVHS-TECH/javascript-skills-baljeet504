0c771c5d0487bfeaee2e25578588b5fd51c1deec
baljeet504 <26527ns@hvhs.school.nz>
2026-06-01 08:39:09 +0000

    Jun 1, 2026, 8:39 AM

--- t14_arrays_1.js
+++ t14_arrays_1.js
@@ -1,2 +1,6 @@
-let chocolateArray = ["You loath chocolate", "Chocolate is meh", "Chocolate is pretty good", "Chocolate is the best thing EVER!!!!"];
+function getchocolate() {
+    const inputVal = document.getElementById('chocolateArray').value;
+    console.log("Input received:", inputVal);
+    
+    let chocolateArray = ["You loath chocolate", "Chocolate is meh", "Chocolate is pretty good", "Chocolate is the best thing EVER!!!!"];
 output.innerHTML = chocolateArray
